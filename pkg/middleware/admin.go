fcb06e4e4f051be6b3c9c2256a77b5c0f8b7f6cf
ivan <nerfel@list.ru>
2025-10-20 00:22:01 +0300

    Added reply for failed IsAdmin middleware

--- pkg/middleware/admin.go
+++ pkg/middleware/admin.go
@@ -2,11 +2,17 @@ package middleware
 
 import (
 	"telefool/pkg/di"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func IsAdmin(next Handler) Handler {
 	return func(ctx *di.UpdateContext) {
 		if ctx.Update.Message.From.UserName != ctx.Conf.AdminUserName {
+			msg := tgbotapi.NewMessage(ctx.Update.Message.Chat.ID, "У вас не достаточно прав для данного функционала")
+			msg.ReplyToMessageID = ctx.Update.Message.MessageID
+			ctx.Bot.Send(msg)
+
 			return
 		}
 
