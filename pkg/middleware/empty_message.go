c457d2ee6e80ef6f288c4fdceeec1836a47f7f0f
ivan <nerfel@list.ru>
2025-11-15 13:26:02 +0300

    Fixed nil pointer dereference

--- pkg/middleware/empty_message.go
+++ pkg/middleware/empty_message.go
@@ -6,7 +6,7 @@ import (
 
 func IgnoreEmpty(next Handler) Handler {
 	return func(ctx *di.UpdateContext, container *di.Container) {
-		if ctx.Update.Message.Text == "" {
+		if ctx.Update.Message == nil || ctx.Update.Message.Text == "" {
 			return
 		}
 
